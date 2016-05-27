e4ba93be56231e659dcc87842c971b7dd81c5486
Denise Ignatova <denise@digitickets.co.uk>
2016-05-27 13:47:44 +0100

    regex

--- src/Omnipay/Alert/Message/PurchaseRequest.php
+++ src/Omnipay/Alert/Message/PurchaseRequest.php
@@ -143,7 +143,7 @@ class PurchaseRequest extends AbstractRequest
         $data->AMEXPurchaseType = '';
         $data->notifyUrl = $this->getNotifyUrl();
         $data->returnUrl = $this->getReturnUrl();
-        $data->UserDefinedField1 = substr(preg_replace('/[\s\(\)]+/','',$this->getDescription()), 0, 20);
+        $data->UserDefinedField1 =  preg_replace('/[A-Z][A-Z\d]+/','',substr(preg_replace('/[\s\(\)]+/','',$this->getDescription()), 0, 20 ));
         $data->CheckoutRequest = '';
 
         return $data;
